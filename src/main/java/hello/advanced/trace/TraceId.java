fdaaa08de3ccc50231de2b5516adcbb7500c63e2
lucia1 <lucia1@pulmuone.com>
2023-02-01 17:36:06 +0900

    commit test

---
+++ src/main/java/hello/advanced/trace/TraceId.java
@@ -0,0 +1,44 @@
+package hello.advanced.trace;
+
+import java.util.UUID;
+
+public class TraceId {
+
+    private String id; // 트랜잭션 아이디
+    private int level; // depth
+
+    public TraceId() {
+        this.id = createId();
+        this.level = 0;
+    }
+
+    private TraceId(String id, int level){
+        this.id = id;
+        this.level = level;
+    }
+
+    private String createId() {
+        return UUID.randomUUID().toString().substring(0,8);
+    }
+
+    public TraceId createNextId(){
+        return new TraceId(id,level+1);
+    }
+
+    public TraceId createPreviousId(){
+        return new TraceId(id,level-1);
+    }
+
+    public boolean isFirstLevel(){
+        return level == 0;
+    }
+
+    public int getLevel(){
+        return level;
+    }
+
+    public String getId(){
+        return id;
+    }
+
+}
